21467873ce247d0422bbd6f131445d8b6d56149e
Harry <harrybest800@gmail.com>
2021-02-21 18:13:55 +0000

    better movement and sprite change when moving

--- main.c
+++ main.c
@@ -4,43 +4,103 @@
 /* import sprites */
 #include "sprites/TinyOctopus.c"
 
+#define DISPLAY_WIDTH 160
+#define DISPLAY_HEIGHT 144
+
+#define DEBUG 0
+
+const enum directions{NORTH, EAST, SOUTH, WEST};
+
+const UINT8 OCTOPUS_SPRITE = 0;
+UINT8 octopusDirection = NORTH;
+INT16 octopusPosition[2] = {80,72};
+
+
+const UINT8 MOVEMENT_SPEED = 1;
+const UINT8 DEFAULT_DELAY = 1;
+const UINT8 MOVEMENT_DELAY = 8;
+
 void main() {
+
     UINT8 currentSpriteIndex = 0;
     UINT8 current_delay = 100;
-    set_sprite_data(0, 2, TinyOctopus); /* starting from tile 0 read in 2 tiles from TinyOctopus into VRAM */
-    set_sprite_tile(0,0); /* set sprite 0 to tile 0 from memory */
-    move_sprite(0, 88, 78); /* move sprite 0 to 88, 78 */
+    
+    /* setup */
+    
+    set_sprite_data(0, 4, TinyOctopus); /* starting from tile 0 read in 2 tiles from TinyOctopus into VRAM */
+    set_sprite_tile(OCTOPUS_SPRITE, 0); /* set sprite 0 to tile 0 from memory */
+    move_sprite(OCTOPUS_SPRITE, octopusPosition[0], octopusPosition[1]); /* move sprite 0 to 88, 78 */
+    
+    
     SHOW_SPRITES;
 
     /* game loop */
     while (1) {
 
-        current_delay = 100;
+        current_delay = MOVEMENT_DELAY;
+
+
+        /* handle input */
+        UINT8 move = 1;
+
         switch(joypad()) {
             case J_LEFT:
-                scroll_sprite(0, -10, 0);
+                octopusDirection = WEST;
                 break;
             case J_RIGHT:
-                scroll_sprite(0, 10, 0);
+                octopusDirection = EAST;
                 break;
             case J_UP:
-                scroll_sprite(0, 0, -10);
+                octopusDirection = NORTH;
                 break;
             case J_DOWN:
-                scroll_sprite(0, 0, 10);
+                octopusDirection = SOUTH;
                 break;
             default:
-                current_delay = 5;
-
-            
-
+                move = 0;
+                current_delay = DEFAULT_DELAY;        
         }
-        
-        /* animate tiny-octopus */
-        currentSpriteIndex = (currentSpriteIndex + 1) % 2;
 
-        set_sprite_tile(0,currentSpriteIndex); /* set sprite 0 to tile 0 from memory */
+        /* if a movement button was pressed */
+        if ( move == 1 ) {
+            switch(octopusDirection) {
+                case NORTH:
+                    octopusPosition[1] = (octopusPosition[1] - MOVEMENT_SPEED);
+                    break;
+                case EAST:
+                    octopusPosition[0] = (octopusPosition[0] + MOVEMENT_SPEED);
+                    break;
+                case SOUTH:
+                    octopusPosition[1] = (octopusPosition[1] + MOVEMENT_SPEED);
+                    break;
+                case WEST:
+                    octopusPosition[0] = (octopusPosition[0] - MOVEMENT_SPEED);
+                    break;
+            }
 
+
+            while (octopusPosition[0] < 0) {
+                octopusPosition[0] += DISPLAY_WIDTH;
+            }
+            while (octopusPosition[0] > DISPLAY_WIDTH + 4) {
+                octopusPosition[0] -= DISPLAY_WIDTH;
+            }
+            while (octopusPosition[1] < 12) {
+                octopusPosition[1] += DISPLAY_HEIGHT;
+            }
+            while (octopusPosition[1] > DISPLAY_HEIGHT + 12) {
+                octopusPosition[1] -= DISPLAY_HEIGHT;
+            }
+        }
+
+        /* select the correct octopus sprite */
+        set_sprite_tile(OCTOPUS_SPRITE, octopusDirection);
+
+        move_sprite(OCTOPUS_SPRITE, octopusPosition[0], octopusPosition[1]);
+
+        if (DEBUG) {
+            printf("\r\n x: %d, y: %d", octopusPosition[0], octopusPosition[1]);
+        }
         delay(current_delay);
     }
 }
