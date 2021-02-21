d57404f299b9742c4852c111bf5ae2301f613111
Harry <harrybest800@gmail.com>
2021-02-21 17:15:05 +0000

    basic movement

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 void main() {
     UINT8 currentSpriteIndex = 0;
-
+    UINT8 current_delay = 100;
     set_sprite_data(0, 2, TinyOctopus); /* starting from tile 0 read in 2 tiles from TinyOctopus into VRAM */
     set_sprite_tile(0,0); /* set sprite 0 to tile 0 from memory */
     move_sprite(0, 88, 78); /* move sprite 0 to 88, 78 */
@@ -14,13 +14,33 @@ void main() {
 
     /* game loop */
     while (1) {
+
+        current_delay = 100;
+        switch(joypad()) {
+            case J_LEFT:
+                scroll_sprite(0, -10, 0);
+                break;
+            case J_RIGHT:
+                scroll_sprite(0, 10, 0);
+                break;
+            case J_UP:
+                scroll_sprite(0, 0, -10);
+                break;
+            case J_DOWN:
+                scroll_sprite(0, 0, 10);
+                break;
+            default:
+                current_delay = 5;
+
+            
+
+        }
         
+        /* animate tiny-octopus */
         currentSpriteIndex = (currentSpriteIndex + 1) % 2;
 
         set_sprite_tile(0,currentSpriteIndex); /* set sprite 0 to tile 0 from memory */
 
-        delay(1000);
-
-        scroll_sprite(0, 10, 0);
+        delay(current_delay);
     }
 }
