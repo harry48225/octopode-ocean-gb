c96ef9a4237c3d72040b137ad4a5950142844f73
Harry <harrybest800@gmail.com>
2021-03-04 22:21:41 +0000

    ability to shoot stationary ink

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 /* import sprites */
 #include "sprites/TinyOctopus.c"
+#include "sprites/InkShots.c"
 
 /* import tiles */
 #include "sprites/BackgroundTiles.c"
@@ -18,7 +19,23 @@
 
 #define DEBUG 0
 
-const enum directions{NORTH, EAST, SOUTH, WEST};
+typedef struct {
+    int x;
+    int y;
+    int x_vel;
+    int y_vel;
+    int direction;
+    int spriteNumber;
+} ink;
+
+typedef struct {
+    ink shots[40];
+    int endIndex;
+} inkList;
+
+const enum directions{NORTH, SOUTH, WEST, EAST, NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST};
+
+inkList inks;
 
 const UINT8 OCTOPUS_SPRITE = 0;
 UINT8 octopusDirection = NORTH;
@@ -29,6 +46,8 @@ const UINT8 DEFAULT_DELAY = 1;
 const UINT8 MOVEMENT_DELAY = 16;
 
 const UINT8 SCROLL_SPEED = 1;
+
+
 
 void update_octopus_position(int * octopusPosition, UINT8 joypad_state) {
     /* takes the octopus position pointer and updates it */
@@ -67,29 +86,40 @@ void update_octopus_sprite(UINT8 joypad_state) {
     
     int octopus_sprite_number = 0;
     
+    joypad_state = joypad_state & (J_UP | J_DOWN | J_LEFT | J_RIGHT);
+
     if (joypad_state == J_UP) {
         octopus_sprite_number = 0;
+        octopusDirection = NORTH;
     }
     else if (joypad_state == J_DOWN) {
         octopus_sprite_number = 1;
+        octopusDirection = SOUTH;
     }
     else if (joypad_state == J_LEFT) {
         octopus_sprite_number = 2;
+        octopusDirection = WEST;
     }
     else if (joypad_state == J_RIGHT) {
         octopus_sprite_number = 3;
+        octopusDirection = EAST;
     }
     else if (joypad_state == (J_UP | J_LEFT)) {
         octopus_sprite_number = 4;
+        octopusDirection = NORTH_WEST;
     }
     else if (joypad_state == (J_UP | J_RIGHT)) {
         octopus_sprite_number = 5;
+        octopusDirection = NORTH_EAST;
+
     }
     else if (joypad_state == (J_DOWN | J_LEFT)) {
         octopus_sprite_number = 6;
+        octopusDirection = SOUTH_WEST;
     }
     else if (joypad_state == (J_DOWN | J_RIGHT)) {
         octopus_sprite_number = 7;
+        octopusDirection = SOUTH_EAST;
     }
         
 
@@ -125,6 +155,55 @@ void scroll_background(UINT8 joypad_state) {
     }
 }
 
+void shoot_ink(int originPosition[], int direction, int x_velocity, int y_velocity) {
+    // Shoots an ink shot originating from the the origin position in the given direction
+    ink new_shot;
+    new_shot.x = originPosition[0];
+    new_shot.y = originPosition[1];
+
+    new_shot.direction = direction;
+
+    new_shot.x_vel = x_velocity;
+    new_shot.y_vel = y_velocity;
+
+
+    /*
+        all of this below needs to be changed to be dynamic
+    */
+
+    new_shot.spriteNumber = 1; // need to implement some sort of dynamic sprite numbering system
+
+    inks.shots[0] = new_shot;
+
+    inks.endIndex = 1;
+
+    /*
+        end dynamic section
+    */
+
+    /*
+        set the correct sprite
+    */
+    
+    /* 
+        increment the end index
+    */
+
+}
+
+void draw_inks() {
+    for (int i = 0; i < inks.endIndex; i++) {
+        // This works because the order the sprites are in VRAM are the same as in the order of the directions in the enum
+        // 8 is the offset to avoid the octopus sprites in memory
+        ink shot;
+        shot = inks.shots[i];
+
+        set_sprite_tile(shot.spriteNumber, 8 + shot.direction); 
+
+        move_sprite(shot.spriteNumber, shot.x, shot.y);
+    }
+}
+
 void setup() {
 
     /* font setup */
@@ -134,11 +213,19 @@ void setup() {
     min_font = font_load(font_min);
     font_set(min_font);
 
-    /* setup */
+    /* logic setup */
+
+    inks.endIndex = 0;
+
+    /* visual setup */
     
+    /* setup sprites */
     set_sprite_data(0, 8, TinyOctopus); /* starting from tile 0 read in 4 tiles from TinyOctopus into VRAM */
     set_sprite_tile(OCTOPUS_SPRITE, 0); /* set sprite 0 to tile 0 from memory */
     move_sprite(OCTOPUS_SPRITE, octopusPosition[0], octopusPosition[1]); /* move sprite 0 to 88, 78 */
+
+
+    set_sprite_data(8, 8, InkShots); // Starting from tile 8 (because the first 8 tiles are tiny octopus) read 8 tiles into VRAM
 
     /* setup background */
     set_bkg_data(37, 11, BackgroundTiles); // load starting at 37 so that they're loading after the fonts
@@ -181,8 +268,12 @@ void main() {
             current_delay = DEFAULT_DELAY;      
         }
 
-        move_sprite(OCTOPUS_SPRITE, octopusPosition[0], octopusPosition[1]);
+        if (joypad_state & (J_A)) {
+            shoot_ink(octopusPosition, octopusDirection, 1, 1);
+        }
 
+        move_sprite(OCTOPUS_SPRITE, octopusPosition[0], octopusPosition[1]);
+        draw_inks();
         if (DEBUG) {
             printf("\r\n x: %d, y: %d", octopusPosition[0], octopusPosition[1]);
         }
