9109894b207092edba72e4747a8b5596044a0ca6
Harry <harrybest800@gmail.com>
2021-03-18 17:00:21 +0000

    basic metasprite class

---
+++ metasprite.c
@@ -0,0 +1,31 @@
+#include "gb/gb.h"
+#include "metasprite.h"
+
+
+void move_metasprite(MetaSprite * sprite, int x, int y) {
+
+    int spriteIds[] = sprite->sprite_ids;
+
+    for (int i = 0; i < 2; i++) {
+
+        for (int j = 0; j < 2; i++) {
+            move_sprite(spriteIds[2*i + j], x + 8*i, y + 8*j);
+        }
+        
+    }
+
+}
+
+void set_metasprite_tiles(MetaSprite * sprite,  int sprite_numbers[4]) {
+    
+    for (int i = 0; i < 4; i++) {
+
+        set_sprite_tile(sprite->sprite_ids[i], sprite_numbers[i]);
+
+    }
+}
+
+void draw_metasprite(MetaSprite * sprite, int x, int y, int sprite_numbers[4]) {
+    set_metasprite_tiles(sprite, sprite_numbers);
+    move_metasprite(sprite, x,y);
+}
---
+++ metasprite.h
@@ -0,0 +1,22 @@
+typedef struct {
+    int sprite_ids[4];
+}  MetaSprite;
+
+/** move MetaSprite to x and y
+
+*/
+void move_metasprite(MetaSprite * sprite, int x, int y);
+
+
+/** sets the metasprites tiles
+ * @param sprite the metasprite to set the tiles of
+ * @param sprite_numbers the numbers of the sprites to use int array of length 4
+*/
+void set_metasprite_tiles(MetaSprite * sprite,  int sprite_numbers[4]);
+
+/** draws a given metasprite at the given x and y with the given sprite numbers
+ * @param x the x-coordinate of the sprite
+ * @param y the y-coordinate of the sprite
+ * @param sprite_numbers an array of length 4 containing the sprite numbers
+*/
+void draw_metasprite(MetaSprite * sprite, int x, int y, int sprite_numbers[4]);
