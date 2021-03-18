7764bb647cdffd33dc7774c3b2920182c92739c3
Harry <harrybest800@gmail.com>
2021-03-18 19:38:00 +0000

    fixed strange loading error

--- main.c
+++ main.c
@@ -60,6 +60,14 @@ const UINT8 SCROLL_SPEED = 1;
 
 Diver diver;
 
+void play_ink_shot_sound() {
+
+    NR41_REG = 0x26;
+    NR42_REG = 0xA1;
+    NR43_REG = 0x31;
+    NR44_REG = 0x80;
+
+}
 
 void update_octopus_position(int * octopusPosition, UINT8 joypad_state) {
     /* takes the octopus position pointer and updates it */
@@ -272,6 +280,13 @@ void initalise_ink_list() {
 
 }
 
+void setup_sound() {
+    // these registers must be in this specific order!
+    NR52_REG = 0x80; // is 1000 0000 in binary and turns on sound
+    NR50_REG = 0x77; // sets the volume for both left and right channel just set to max 0x77
+    NR51_REG = 0xFF; // is 1111 1111 in binary, select which chanels we want to use in this case all of them. One bit for the L one bit for the R of all four channels
+}
+
 void setup() {
 
     /* font setup */
@@ -330,21 +345,7 @@ void setup() {
     DISPLAY_ON;
 }
 
-void setup_sound() {
-    // these registers must be in this specific order!
-    NR52_REG = 0x80; // is 1000 0000 in binary and turns on sound
-    NR50_REG = 0x77; // sets the volume for both left and right channel just set to max 0x77
-    NR51_REG = 0xFF; // is 1111 1111 in binary, select which chanels we want to use in this case all of them. One bit for the L one bit for the R of all four channels
-}
 
-void play_ink_shot_sound() {
-
-    NR41_REG = 0x26;
-    NR42_REG = 0xA1;
-    NR43_REG = 0x31;
-    NR44_REG = 0x80;
-
-}
 
 void main() {
 
