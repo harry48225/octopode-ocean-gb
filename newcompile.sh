3da12d8a16426151871d353e924224a36ff7893f
Harry <harrybest800@gmail.com>
2021-03-19 09:46:00 +0000

    updated compile script

--- newcompile.sh
+++
@@ -1,4 +0,0 @@
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -c -o diver.o diver.c
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -c -o metasprite.o metasprite.c
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -c -o main.o main.c
-/opt/gbdk/bin/lcc -Wa-l -Wl-m -Wl-j -o main.gb main.o diver.o metasprite.o
